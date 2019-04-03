e2a69707a9682022c1a9ef9d5038b612557e8396
Siege211 <Siege211@morris.umn.edu>
2019-04-03 16:50:04 -0500

    commented out sample websocket subscription call

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { RpcClient } from "tendermint";
+const client = RpcClient("wss://rpc.cosmos.network:26657");
+client.subscribe({ query: "tm.event = 'NewBlock'" }, event => {
+console.log(event);
+});
 
 class App extends React.Component {
   constructor(props) {
@@ -14,8 +19,8 @@ class App extends React.Component {
   	return (
   	<div>
       React Renders
-      {this.state.blockList.map(yo=>
-        <p>{yo}</p>
+      {this.state.blockList.map( (yo,i) =>
+        <p key={i}>{yo}</p>
       )}
   	</div>
   	)
@@ -24,3 +29,18 @@ class App extends React.Component {
 
 export default App;
 
+//to do:
+  // instead of rendering regular elements in the .map, render instances of a "block" 
+  // component which i will define in a seperate file
+
+
+
+  //possible approach
+    // find the current height to determinute the 10 most recent blocks
+    // possibly the 10 highest is the same as 10 most recent
+
+
+    // // i need to a way to access the "last height" variable so 
+    // // that it becomes part of the query in this url:
+    //     https://rpc.cosmos.network:26657/blockchain?minHeight=0&maxHeight=10
+    // the maxx height will be "last height" and the min height will be that minus 10
