35552383d36591d1dddc1bdbd829602c4dab485b
Siege211 <Siege211@morris.umn.edu>
2019-04-02 23:05:05 -0500

    Initialized with blank parent class component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+  	return (
+  	<div>
+      <InputForm />
+  	</div>
+  	)
+  }
+}
+
+export default App;
+
