8b30e6b846d88198e122ebdf00dd6195231cac94
Siege211 <Siege211@morris.umn.edu>
2019-04-02 23:56:48 -0500

    App dynamically renders list items from state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,13 +3,20 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      blockList: ["pretest 1","pretest2"]
+    }
   }
-
+    componentWillMount() {
+    this.setState({blockList: ["test 1","test2"]})
+  }
   render() {
   	return (
   	<div>
       React Renders
+      {this.state.blockList.map(yo=>
+        <p>{yo}</p>
+      )}
   	</div>
   	)
   }
