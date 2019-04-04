ac38b52aee75211ea457b0287a6f63a28b712878
Siege211 <Siege211@morris.umn.edu>
2019-04-03 20:02:27 -0500

    Component is styled

--- client/src/components/Block.jsx
+++ client/src/components/Block.jsx
@@ -1,3 +1,33 @@
 import React from 'react';
-const Block = props => <div>{props.children}</div>;
+const Block = props => 
+<div style={{marginBottom:"10px",display:"flex",flexDirection:"column",border:"2px solid black"}}>
+	
+	<span style={{marginBottom:"2px",display:"flex",flexDirection:"row"}}>Block Id: {props.blockId && 
+		<div>{props.blockId.hash}</div>}
+	</span>
+
+	<span style={{marginBottom:"2px",display:"flex",flexDirection:"row"}}>
+	Time Created: 
+	{props.blockHeader && 
+		<div>{props.blockHeader.time}</div>}
+	</span>
+
+	<span style={{display:"flex",flexDirection:"row"}}>
+	Height: {props.blockHeader && 
+		<div>{props.blockHeader.height}</div>}
+	</span>
+
+</div>;
+
+
 export default Block;
+
+
+
+/// things for info page to include:
+	// total_txs
+	// num_txs
+	// proposer_address
+	// consensus_hash
+	// validators_hash
+	// heightblock_id.hash
