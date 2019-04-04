6f1fca70dbfba913f3f0cd7c069f0f5e97004753
Siege211 <Siege211@morris.umn.edu>
2019-04-04 17:49:39 -0500

    logs req.query on blockview route

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/blockDetailsView:*',function (req, res) {
+	console.log(req.query);
+  res.redirect('http://google.com');
+})
+//dynamically accepts a block id
+// fetches that block
+// redirects to a block view page
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
